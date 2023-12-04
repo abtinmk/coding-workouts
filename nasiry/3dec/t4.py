b62397413645c51134cb0b7b5e73bea4b8a4402b
mohali <mohali4h@gmail.com>
2023-12-04 09:43:18 +0330

    change the name of 2 and 4

--- nasiry/3dec/t4.py
+++ nasiry/3dec/t4.py
@@ -1,29 +1,34 @@
-memory = "Ya Hossein   " * 100 
 
-a = input ()
+memory = "In the name Of Allah"
+# writting islamic things in memory to make or computers safe against satan
+
+a = int (input ())
 b = {}
-aa = list(a)
-def deleteL (theList,letter):
-    while letter in theList :
-        theList.remove(letter)
-for letter in ' !,?/=+_-)(\{\}\\.*&;:':
-    deleteL(aa,letter)
-for letter in aa :
-    b [letter] = aa.count(letter)
-c = list(b.items())
-c.sort(key=lambda x:x[1])
-c.reverse()
-bests = []
-maxi = c[0][1]
-for x in c:
-    if x[1] == maxi:
-        bests.append(c)
-    else:
-        break
-lets = []
-for letter, _ in c:
-    lets.append(letter.upper()) 
-print(' '.join(lets))
-print(maxi)
+c = {}
+for i in range (a):
+    key = int (input ())
+    value = input().split ()
+    b[key] = value
+    c[key] = value.copy()
+b_iter = list(b.keys())
+b_iter.sort()
+b_iter.reverse()
+
+for n, i in  enumerate(b_iter):
+    iteriter = b_iter.copy()
+    iteriter = iteriter[n+1:]
+    for j in iteriter:
+        for item in b[i] :
+            while item in c[j] : c[j].remove(item)
+for key ,row in c.items():
+    for num, item in enumerate (row):
+        for chnum, check in enumerate(row[num+1:]):
+            if check == item :
+                row[chnum+num+1] = None
+for _, row in c.items():
+    while None in row:
+        row.remove(None)
+
+print (c)
 for t in memory: 
     g = t * 3
