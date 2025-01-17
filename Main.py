79d983affed363d93a5acb1ffe61908ba9f20cb0
abtin <151994440+abtinmk@users.noreply.github.com>
2025-01-17 22:15:37 +0330

    Add files via upload

--- Main.py
+++ Main.py
@@ -29,7 +29,7 @@ y_val_pred=model.predict(x_val)
 print(f'accuracy random forest:{accuracy_score(y_val,y_val_pred):.2f}')
 print(f'confusion matrix random forest:\n{confusion_matrix(y_val,y_val_pred)}')
 print(f'classifiction report:\n',classification_report(y_val,y_val_pred,zero_division=0))
-#random forest is not a good model
+#random forest
 from sklearn.neural_network import MLPClassifier
 #accuracy after training for diffrent activation func: logostic>tanh=relu=identity
 model=MLPClassifier(hidden_layer_sizes=(256,128,64),activation='logistic',solver='adam',max_iter=500,random_state=21,verbose=True)
@@ -38,7 +38,7 @@ y_val_pred=model.predict(x_val)
 print(f'accuracy mlp classifier:{accuracy_score(y_val,y_val_pred):.2f}')
 print(f'confusion matrix mlp classifier:\n{confusion_matrix(y_val,y_val_pred)}')
 print(f'classifiction report:\n',classification_report(y_val,y_val_pred,zero_division=0))
-# MLP not good
+# MLP 
 from sklearn.svm import SVC
 model=SVC(kernel='rbf',C=1 , random_state=21)
 model.fit(x_train,y_train)
@@ -46,35 +46,52 @@ y_val_pred=model.predict(x_val)
 print(f'accuracy svm:{accuracy_score(y_val,y_val_pred):.2f}')
 print(f'confusion matrix svm:\n{confusion_matrix(y_val,y_val_pred)}')
 print(f'classifiction report:\n',classification_report(y_val,y_val_pred,zero_division=0))
-#svm very bad
-import xgboost as xgb
-model=xgb.XGBClassifier(n_estimators=100,learning_rate=0.1,max_depth=6,random_state=42)
-model.fit(x_train,y_train)
-y_val_pred=model.predict(x_val)
-print(f'accuracy xgboost:{accuracy_score(y_val,y_val_pred):.2f}')
-print(f'confusion matrix xgboost:\n{confusion_matrix(y_val,y_val_pred)}')
-print(f'classifiction report:\n',classification_report(y_val,y_val_pred,zero_division=0))
+#svm
 from sklearn.neighbors import KNeighborsClassifier
-model=KNeighborsClassifier(n_neighbors=10)
+model=KNeighborsClassifier(n_neighbors=10)#more than 10 is going to overfiting
 model.fit(x_train,y_train)
 y_val_pred=model.predict(x_val)
 print(f'accuracy KNN:{accuracy_score(y_val,y_val_pred):.2f}')
 print(f'confusion matrix KNN:\n{confusion_matrix(y_val,y_val_pred)}')
 print(f'classifiction report:\n',classification_report(y_val,y_val_pred,zero_division=0))
-#KNN:a little better than mlp and svm but not good
+#KNN:a little better than svm but not good
 #it seems the classical models can not creating a good model for this task
-#for this task it seems we need use a complicated model
+#for this task it seems we need use a complicated model like CNN and from my searchs the code is
+#going to be like this
 import tensorflow as tf
 import keras as ks
 num_classes=5
-
-model=tf.keras.Sequential([
-  tf.keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=(51,59,1)),
-  tf.keras.layers.MaxPooling2D((2,2)),
-  tf.keras.layers.Conv2D(64,(3,3),activation='relu'),
-  tf.keras.layers.MaxPooling2D((2,2)),
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(128,activation='relu'),
-  tf.keras.layers.Dropout(0.5),
-  tf.keras.layers.Dense(num_classes,activation='softmax')
+x=x.reshape(x.shape[0],51,59,1)
+x=x.astype('float32')/np.max(x)
+x_train,x_temp,y_train,y_temp=train_test_split(x,y,test_size=0.3,stratify=y,random_state=21)
+x_val,x_test,y_val,y_test=train_test_split(x_temp,y_temp,test_size=0.5,random_state=21)
+y_train=ks.utils.to_categorical(y_train,num_classes)
+y_test=ks.utils.to_categorical(y_test,num_classes)
+y_val=ks.utils.to_categorical(y_val,num_classes)
+model=ks.Sequential([
+  ks.layers.Conv2D(32,(3,3),activation='relu',input_shape=(51,59,1)),
+  ks.layers.MaxPooling2D((2,2)),
+  ks.layers.Conv2D(64,(3,3),activation='relu'),
+  ks.layers.MaxPooling2D((2,2)),
+  ks.layers.Flatten(),
+  ks.layers.Dense(128,activation='relu'),
+  ks.layers.Dropout(0.5),
+  ks.layers.Dense(num_classes,activation='softmax')
 ]) 
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+history=model.fit(x_train,y_train,epochs=20,batch_size=32,validation_data=(x_val,y_val),verbose=1)
+val_loss , val_acc=model.evaluate(x_val,y_val,verbose=0)
+print(f'val accuracy for CNN:{val_acc:.2f}')
+y_pred=model.predict(x_val)
+y_pred_classes=np.argmax(y_pred,axis=1)
+y_val_classes=np.argmax(y_val,axis=1)
+print(f'confusion matrix CNN:\n{confusion_matrix(y_val_classes,y_pred_classes)}')
+print(f'classifiction report:\n',classification_report(y_val_classes,y_pred_classes,zero_division=0))
+# دقت این مدل نیز چندان فرقی نکرد
+#ارزیابی داده های تست
+y_test_pred=model.predict(x_test)
+y_predtest_classes=np.argmax(y_test_pred,axis=1)
+y_test_classes=np.argmax(y_test,axis=1)
+print(f'confusion matrix :\n{confusion_matrix(y_test_classes,y_predtest_classes)}')
+print(f'classifiction report:\n',classification_report(y_test_classes,y_predtest_classes,zero_division=0))
+#test Accuracy = 52%
